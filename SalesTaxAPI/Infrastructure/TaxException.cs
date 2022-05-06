c7fb751e49509159cc5a6c65000c177fbf300653
Gedeon <mygedi@gmail.com>
2022-05-06 04:11:01 -0400

    Addinng missing files

---
+++ SalesTaxAPI/Infrastructure/TaxException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+namespace SalesTaxClientAPI.Infrastructure
+{
+    [Serializable]
+    public class TaxException : ApplicationException
+    {
+        public HttpStatusCode HttpStatusCode { get; set; }
+        public TaxError TaxjarError { get; set; }
+
+        public TaxException(HttpStatusCode statusCode, TaxError taxError, string message) : base(message)
+        {
+            HttpStatusCode = statusCode;
+            TaxjarError = taxError;
+        }
+    }
+
+    public class TaxError
+    {
+        [JsonProperty("error")]
+        [JsonPropertyName("error")]
+        public string Error { get; set; }
+
+        [JsonProperty("detail")]
+        [JsonPropertyName("detail")]
+        public string Detail { get; set; }
+
+        [JsonProperty("status")]
+        [JsonPropertyName("status")]
+        public string StatusCode { get; set; }
+    }
+}
---
+++ SalesTaxAPI/Services/TaxCalculatorService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SalesTaxClientAPI.Model;
+using SalesTaxClientAPI.Infrastructure;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+
+namespace SalesTaxClientAPI.Services
+{
+    public class TaxCalculatorService : ITaxCalculatorService
+    {
+        private HttpClient _httpContent { get; }
+        private IConfiguration _Configuration { get; }
+
+        public TaxCalculatorService(HttpClient httpContent, IConfiguration configuration)
+        {
+            _Configuration = configuration;
+            httpContent.BaseAddress = new Uri(_Configuration.GetSection("API:Url").Value);
+            httpContent.DefaultRequestHeaders.Accept.Clear();
+            httpContent.DefaultRequestHeaders.Add("Authorization", $"Bearer {_Configuration.GetSection("API:Key").Value}");
+            httpContent.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpContent = httpContent;
+        }
+
+        public async Task<RateResponse> GetTaxRateForAlocation(Rate rate)
+        {
+            string endpoint = $"{_Configuration.GetSection("API:Version").Value}{_Configuration.GetSection("API:TaxRatelocationEndPoint").Value}";
+            string parameters = $"?zip={rate.Zip}&country={rate.Country}&state={rate.State}&city={rate.City}&street={rate.Street}";
+            HttpResponseMessage response = await _httpContent.GetAsync($"{endpoint}{parameters}").ConfigureAwait(false);
+            string returnMessage = await response.Content.ReadAsStringAsync();
+
+            if ((int)response.StatusCode >= 400)
+            {
+                TaxError taxError = JsonConvert.DeserializeObject<TaxError>(returnMessage);
+                string errorMessage = taxError.Error + " - " + taxError.Detail;
+                throw new TaxException(response.StatusCode, taxError, errorMessage);
+            }
+
+            return JsonConvert.DeserializeObject<RateResponse>(returnMessage); ;
+        }
+
+
+
+        public async Task<TaxResponse> CalculateTaxesForAnorder(Tax tax)
+        {
+            string endpoint = $"{_Configuration.GetSection("API:Version").Value}{_Configuration.GetSection("API:CalculateTaxesForAnorderEndPoint").Value}";
+            StringContent requestContent = new StringContent(JsonConvert.SerializeObject(tax), System.Text.Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _httpContent.PostAsync(endpoint, requestContent).ConfigureAwait(false);
+            string returnMessage = await response.Content.ReadAsStringAsync();
+
+            if ((int)response.StatusCode >= 400)
+            {
+                TaxError taxError = JsonConvert.DeserializeObject<TaxError>(returnMessage);
+                string errorMessage = taxError.Error + " - " + taxError.Detail;
+                throw new TaxException(response.StatusCode, taxError, errorMessage);
+            }
+
+            return JsonConvert.DeserializeObject<TaxResponse>(returnMessage); ;
+        }
+    }
+}
